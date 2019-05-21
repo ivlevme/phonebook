a48fce55335574391ef908e1b91bd81f86c1defb
ivlevme <ivlev.me@gmail.com>
2019-05-21 19:38:57 +0300

    поиск

--- src/List.vue
+++ src/List.vue
@@ -1,14 +1,16 @@
 <template>
     <div>
-        <div  v-mydirective="[onePeople.favourite, flag]" v-for="(onePeople, index) in book" :key="onePeople.id" class="name">
-            Имя: {{book[index].name}}  Фамилия: {{book[index].surname}} Отчество - {{book[index].otv}} Телефон - {{book[index].tel}} Избранное - {{book[index].favourite}}
+        <div  v-mydirective="[onePeople.favourite, flag]" v-for="(onePeople, index) in filtered" :key="onePeople.id" class="name">
+            Имя: {{onePeople.name}}  Фамилия: {{onePeople.surname}} Отчество - {{onePeople.otv}} Телефон - {{onePeople.tel}} Избранное - {{onePeople.favourite}}
             <button @click="addToFavourite(index)">{{book[index].favourite == true ? 'Удалить из избранного' : 'Добавить в избранное'}}</button>
             <button @click="deleteItem(index)">Удалить</button>
             <br>
         </div>
         <button @click="flag = !flag">
-          {{flag == false ? 'Подсветить избранные контакты' : 'Убрать подсветку избарнных'}}
+          {{flag == false ? 'Подсветить избранные контакты' : 'Убрать подсветку избранных'}}
         </button>
+        <input type="text" v-model="search" name="" value="">
+        {{search}}
     </div>
 </template>
 
@@ -19,7 +21,8 @@ export default {
       return {
         textButton: 'Добавить в избранное',
         textButtonStatus: false,
-        flag: false
+        flag: false,
+        search: ''
       }
     },
   props: {
@@ -29,25 +32,29 @@ export default {
       book: Array
   },
     methods: {
-        addToFavourite: function (index) {
-            this.book[index].favourite = !this.book[index].favourite;
-
-                // Сортировка по избранному
-            this.book.sort(function (a,b) {
-                if (a.favourite === true) {
-                    return -1;
-                }
-                if (a.favourite === false) {
-                    return 1
-                }
-                if (a.favourite === true && b.favourite === true) {
-                    return 0
-                }
-            })
-        },
-        deleteItem: function (index) {
-            this.$emit('deleteItem', index)
-        }
-    }
+      addToFavourite: function (index) {
+        this.book[index].favourite = !this.book[index].favourite;
+            // Сортировка по избранному
+        this.book.sort(function (a,b) {
+            if (a.favourite === true) {
+                return -1;
+            }
+            if (a.favourite === false) {
+                return 1
+            }
+            if (a.favourite === true && b.favourite === true) {
+                return 0
+            }
+        })
+      },
+      deleteItem: function (index) {
+          this.$emit('deleteItem', index)
+      }
+    },
+   computed: {
+     filtered() {
+       return this.search === "" ? this.book : this.book.filter(onePeople => onePeople.name.toLowerCase().indexOf(this.search.toLowerCase(),) !== -1)
+     }
+   }
 }
 </script>
